250323fedef2d655f088153bcb4344a3c8a14df8
Samuel <samuelnicolasalvesdelima10@gmail.com>
2026-09-23 20:04:46 -0300

    exercicios aula 8 até o 21

---
+++ GUANABARA/MUNDO01/08/desafio19.py
@@ -0,0 +1,4 @@
+import random
+nome = ["Samuel","Jorge","Flavio","Luiz"]
+escolhido = random.choice(nome)
+print("O aluno escolhido foi",escolhido)
---
+++ GUANABARA/MUNDO01/08/desafio20.py
@@ -0,0 +1,5 @@
+from random import shuffle
+nome = ["Samuel","Nikolas","Flavio","Luiz"]
+shuffle(nome)
+
+print("a ordem de apresentação será", nome)
