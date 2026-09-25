81ed4fdd873a103a753cdd73abd1185c99d208fd
Samuel <samuelnicolasalvesdelima10@gmail.com>
2026-09-24 23:24:08 -0300

    termienei a prática da aula!

--- GUANABARA/MUNDO01/pythontest/aula09.py
+++ GUANABARA/MUNDO01/pythontest/aula09.py
@@ -1,12 +1,25 @@
+from gettext import find
+
 frase = "Curso em Video Python"
 print(frase[1:15])
 
-print("Oi")
+print(frase.upper().count('o')) #quantos "o" tem
 
-print(""""Nessa aula, vamos aprender operações com String no Python.
-As principais operações que vamos aprender são o Fatiamento de String, 
-Análise com len(), count(), find(), transformações com 
-replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
-""""")
+print(frase.replace('Curso',"Android")) #substitui palavras
+
+print(len(frase.strip())) #quantas letras tem
+
+print(frase.lower().find('video'))
+
+dividido = frase.split() #cria lista
+
+print(dividido[2][3])
+
+print("Oi\n")
+print("""Nessa aula, vamos aprender operações com String no Python.
+As principais operações que vamos aprender são o Fatiamento de String,
+Análise com len(, count(), find(), transformações com
+replae(), upper), lower(), capitalize(), title(), strip(), junção com join().
+""")
 
 print(frase.upper().count('0'))
