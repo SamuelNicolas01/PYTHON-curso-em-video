fe4c9cde8650117864d2f437dfef390139af16b1
Samuel <samuelnicolasalvesdelima10@gmail.com>
2026-09-24 22:09:21 -0300

    não terminei ainda!

---
+++ GUANABARA/MUNDO01/pythontest/aula09.py
@@ -0,0 +1,12 @@
+frase = "Curso em Video Python"
+print(frase[1:15])
+
+print("Oi")
+
+print(""""Nessa aula, vamos aprender operações com String no Python.
+As principais operações que vamos aprender são o Fatiamento de String, 
+Análise com len(), count(), find(), transformações com 
+replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+""""")
+
+print(frase.upper().count('0'))
